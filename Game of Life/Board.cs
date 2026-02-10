83b8d803bc6cb0af01d461c849e740566ca74ac8
Aaron Bataeff <aaron.bataeff.21@gmail.com>
2026-02-10 04:47:59 -0600

    Add project files.

---
+++ Game of Life/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Board
+{
+    private bool[,] grid;
+    private int rows;
+    private int cols;
+
+    public Board(int rows, int cols)
+    {
+        this.rows = rows;
+        this.cols = cols;
+        grid = new bool[rows, cols];
+    }
+}
