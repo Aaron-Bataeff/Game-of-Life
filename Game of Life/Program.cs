fb6999d69b38cb8d0ead328fdab49d27528526bb
Aaron Bataeff <aaron.bataeff.21@gmail.com>
2026-02-10 04:55:42 -0600

    Added basic board test in Program.cs

--- Game of Life/Program.cs
+++ Game of Life/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Board board = new Board(5, 5);
+
+        Console.WriteLine("Initial board:");
+        board.Print();
+
+        Console.WriteLine("\nClearing board...");
+        board.Clear();
+
+        Console.WriteLine("\nBoard after clear:");
+        board.Print();
+    }
+}
